9bd4567475d326233fedf2a07957a9bbde37c47f
Phil Fluegel <pfluegelcx@gmail.com>
2016-01-15 09:17:14 -0600

    stuck on incoming

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,5 +3,12 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    puts "INCOMING PARAMS: #{params}"
+
+    @user = User.find(params[:sender])
+    @topic = Topic.find(params[:subject])
+
+    unless @topic.exists?
+
 
 end
