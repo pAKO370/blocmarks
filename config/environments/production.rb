67e3c4e5a1509fb41367d5ff252b3037f80da743
Phil Fluegel <pfluegelcx@gmail.com>
2016-01-15 14:46:26 -0600

    set production mailer base url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'infinite-anchorage-3954.herokuapp.com' }
 end
