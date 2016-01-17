e84e66da45c4c8ce93bf00c20e6ff04844114ac6
Phil Fluegel <pfluegelcx@gmail.com>
2016-01-17 16:29:38 -0600

    More production env changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'infinite-anchorage-3954.herokuapp.com' }
+   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
