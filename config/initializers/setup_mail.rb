48da0cc47a8cd401ce96fbd22f4e77f4d3f57e44
Phil Fluegel <pfluegelcx@gmail.com>
2016-01-18 14:27:29 -0600

    refactored to use figaro

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address                    => 'smtp.sendgrid.net',
   :port                       => '2525',
   :authentication             => :plain,
-  :user_name                  => 'app45863107@heroku.com',
-  :password                   =>  'sbec41py9147',
+  :user_name                  => ENV['SENDGRID_USERNAME'],
+  :password                   => ENV['SENDGRID_PASSWORD'],
   :domain                     => 'heroku.com',
   :enable_starttls_auto       => true
 }
