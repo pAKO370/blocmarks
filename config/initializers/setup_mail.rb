0ce7122de2a717fa3b973b8008c5786f91e6ead0
Phil Fluegel <pfluegelcx@gmail.com>
2016-01-26 08:42:47 -0600

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address                    => 'smtp.sendgrid.net',
-  :port                       => '2525',
+  :port                       => '587',
   :authentication             => :plain,
-  :user_name                  => 'app45611129@heroku.com',
-  :password                   =>  't0sbn7ut4560',
+  :user_name                  => ENV['SENDGRID_USERNAME'],
+  :password                   => ENV['SENDGRID_PASSWORD'],
   :domain                     => 'heroku.com',
   :enable_starttls_auto       => true
 }
